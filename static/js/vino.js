c6ff2b3f34d72cf25be3721c00758e080801c273
Alex <aadamson@stanford.edu>
2013-07-11 04:30:21 -0500

    finished documentation for the night

--- static/js/vino.js
+++ static/js/vino.js
@@ -1,77 +1,53 @@
+/**
+ * Function: getRowHTML
+ * --------------------
+ * Given a JSON record associated with a tweet referencing a Vine, 
+ * parses relevant members (abbreviated URL, prof. pic, text), and 
+ * constructs and returns an HTML table row.
+ */
+
+var getRowHTML = function(record) {
+	var picline = '<img width=\'48px\' height=\'48px\' src="' + record['user']['profile_image_url'] + '">';
+	var textline = record['text'].replace(record['entities']['urls'][0]['url'], 
+										  '<a href="' + record['entities']['urls'][0]['url'] + '">' 
+										  + record['entities']['urls'][0]['url'] +'</a>');
+	textline += '<br>' + record['user']['screen_name'];
+	
+	var innerHTML = '<td class="profile-pic">' + picline + '</td>';
+	innerHTML += '<td class="tweet-text">' + textline + '</td>';
+	return innerHTML;
+};
+
+/**
+ * Function: updateTable
+ * ---------------------
+ * Given a JSON record associated with a tweet referencing a Vine, 
+ * finds the table in document with the id 'vidtable', inserts a row,
+ * collects HTML for that row from getRowHTML, and deletes the last 
+ * row if necessary.
+ */
+
+var updateTable = function(record) {
+	var historyTable = document.getElementById('vidtable');
+	var newRow = historyTable.insertRow(0);
+	newRow.innerHTML = getRowHTML(record);
+	
+	if(historyTable.rows.length > 10) {
+		historyTable.deleteRow(historyTable.rows.length - 1);
+	}
+	
+};
+
 var Vino = (function($) {
     var video =  _V_("v");
-    videojs("v").ready(function(){
-		console.log("loaded");
-		var player = this;
-	
-		var videoEnd = function(){
-			app.draw();
-			
-			console.log('ended')
-		};
-		player.on("ended", videoEnd);
-		
-		var videoErr = function(){
-			var table = document.getElementById('vidtable');
-			table.deleteRow(0);
-						
-			app.draw();
-			console.log('error')
-		};
-		player.on("error", videoErr);
-		
-		var videoLoaded = function(){
-			video.play();
-		};
-		player.on("loadeddata", videoLoaded);
-	});
-	
-	/**
-	 * Function: getRowHTML
-	 * --------------------
-	 * Given a JSON record associated with a tweet referencing a Vine, 
-	 * parses relevant members (abbreviated URL, prof. pic, text), and 
-	 * constructs and returns an HTML table row.
-	 */
-	
-	var getRowHTML = function(record) {
-		var picline = '<img width=\'48px\' height=\'48px\' src="' + record['user']['profile_image_url'] + '">';
-		var textline = record['text'].replace(record['entities']['urls'][0]['url'], 
-											  '<a href="' + record['entities']['urls'][0]['url'] + '">' 
-											  + record['entities']['urls'][0]['url'] +'</a>');
-		textline += '<br>' + record['user']['screen_name'];
-		
-		var innerHTML = '<td class="profile-pic">' + picline + '</td>';
-		innerHTML += '<td class="tweet-text">' + textline + '</td>';
-		return innerHTML;
-	};
-	
-	/**
-	 * Function: updateTable
-	 * ---------------------
-	 * Given a JSON record associated with a tweet referencing a Vine, 
-	 * finds the table in document with the id 'vidtable', inserts a row,
-	 * collects HTML for that row from getRowHTML, and deletes the last 
-	 * row if necessary.
-	 */
-	
-	var updateTable = function(record) {
-		var historyTable = document.getElementById('vidtable');
-		var newRow = historyTable.insertRow(0);
-		newRow.innerHTML = getRowHTML(record);
-		
-		if(historyTable.rows.length > 10) {
-			historyTable.deleteRow(historyTable.rows.length - 1);
-		}
-		
-    };
     
     /**
      * membrane
      * --------
      * Used as an object that serves as a go-between, making API calls
      * at user input, and processing changes of state so they can be
-     * reflected in the browser.
+     * reflected in the browser. processUpdates is the only function
+     * intended for use as a callback
      */
 	
     var membrane = function(options) {
@@ -80,14 +56,23 @@ var Vino = (function($) {
         this.load(true);
     };
     
-	var load_size = 25;
+	var load_size = 20;
 	
     membrane.prototype = {
-		load: function(drawOnResponse) {
+		/**
+		 * Function: load
+		 * --------------
+		 * Constructs a URL from this.options and leverages jQuery to 
+		 * make an API call that returns a JSON array to the callback function.
+		 * The response is passed off to queue and possibly to processUpdates
+		 * for immediate processing of the JSON records for use in updating
+		 * the document.
+		 */
+		load: function(processUpdatesOnResponse) {
             if (this.options.recent) {
                 endpoint = this.generateEndpointURL('recent');
             } else {
-                endpoint = this.generateTagURL(this.options.tag);
+                endpoint = this.generateTagURL(this.options.tag); 
             }
             
             membraneObject = this;
@@ -96,29 +81,47 @@ var Vino = (function($) {
 				console.log('api call made');
                 membraneObject.queue(response);
 
-				if(drawOnResponse){
-					console.log("load calling draw...");
-					membraneObject.draw();
+				if(processUpdatesOnResponse){
+					console.log("load calling processUpdates...");
+					membraneObject.processUpdates();
 				}
             }, 'json');
         },
 		
+		/**
+		 * Function: queue
+		 * ---------------
+		 * Checks to see if the array of JSON records passed to it (data)
+		 * has any records, and appends the records to this._queue if it does.
+		 */
+		
 		queue: function(data) {
 			if (data.length == 0) {
 				console.log("Null parameter passed as data to queue");
-                //this.load(this._queue.length == 0);
-                return false;
+                return; // load will attempt to processUpdates, which will 
+						// recognize lack of records and call load again
             }
-			
-            var q = this._queue;
+		
             this._queue = this._queue.concat(data);
-            
         },
-
-        draw: function() {			
+		
+		/**
+		 * Function: processUpdates
+		 * --------------
+		 * After determining that the queue is not empty (and calling
+		 * load and returning if it is), updates the source of the video.js
+		 * object and calls updateTable if the last video in the queue has
+		 * a videoURL attached to it. Otherwise, pops the URL-less JSON record
+		 * off the top of the queue and calls processUpdates again to try to process the
+		 * next record.
+		 * Before returning, the function checks to see if there are less than five
+		 * records in the queue and loads more if there are.
+		 */
+        processUpdates: function() {			
             var q = this._queue;
             if(q.length == 0) {
-				this.load(true);
+				this.load(true); // bug: obscure tags timeout here because load_size is larger than the number of recent vines for a 
+								 // particular tag
 				return;
 			}
 			
@@ -131,12 +134,12 @@ var Vino = (function($) {
 			}
 			else {
 				q.pop();
-				this.draw();
+				this.processUpdates();
 				return;
 			}
 
             if (q.length < 5) {
-				console.log("After executing draw, q.length < 5, calling load");
+				console.log("After executing processUpdates, q.length < 5, calling load");
 				this.load(false);
 				console.log("New q.length is " + q.length);
             }
@@ -145,6 +148,8 @@ var Vino = (function($) {
         /**
          * Function: generateEndpointURL
          * -----------------------------
+         * Given endpoint, constructs the path of an API call to be made
+         * by jQuery.
          */
         generateEndpointURL: function(endpoint) {
             var origin = document.location.origin;
@@ -152,6 +157,14 @@ var Vino = (function($) {
             console.log(origin + '/api/' + endpoint + '/' + load_size);
             return origin + '/api/' + endpoint + '/' + load_size;
         },
+        
+        /**
+         * Function: generateTagURL
+         * ------------------------
+         * Given a tag string, appends it to 'tags/' and passes the appended
+         * string to generateEndpointURL for further appendages before
+         * returning the string.
+         */
 
         generateTagURL: function(tag) {
 			var endpoint = 'tags/' + encodeURIComponent(tag);
